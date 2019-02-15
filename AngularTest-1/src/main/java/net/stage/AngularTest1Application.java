d6de7a052ec530ceb38d624ab79491e27d105d26
maryame <maryame.bouhajeb@gmail.com>
2019-02-15 11:05:00 +0800

    first commit

---
+++ AngularTest-1/src/main/java/net/stage/AngularTest1Application.java
@@ -0,0 +1,14 @@
+package net.stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularTest1Application.class, args);
+	}
+
+}
+
